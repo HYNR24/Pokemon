e8381600f57e4613c68b6f8caa5f6bd4fecace72
HYNR241 <hynr241@outlook.com>
2026-02-27 17:48:21 -0500

    Subo proyecto tal cual está

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import {Data} from './services/data';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('pokemon');
+
+protected readonly title = signal('pokemon');
+personajes: any[] = [];
+
+constructor(private Service: Data) {
+  this.imprimirpersonaje();
+}
+
+imprimirpersonaje() {
+  this.Service.obtenerPersonajes().subscribe((resultado) => {
+
+    this.personajes = resultado.results.map((p: any) => {
+
+      const id = p.url.split('/').filter(Boolean).pop();
+
+      return {
+        name: p.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+      };
+
+    });
+
+    console.log(this.personajes);
+  });
+}
 }
